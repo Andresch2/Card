b29b240c3001fecb2e32b1b33ba866f65d56f578
nelson.chaves@camousucc.edu.co <nelson.chaves@campusucc.edu.co>
2024-10-30 09:28:53 -0500

    Agregar cambios

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <main className="bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen flex items-center justify-center">
       <article className="flex flex-col items-center text-white bg-black p-6 md:p-10 rounded-lg shadow-lg space-y-6 md:space-y-6 lg:space-y-0 md:flex-col lg:flex-row lg:space-x-6 max-w-xs md:max-w-lg lg:max-w-2xl">
         
-        <section className="w-32 -ml-19 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-48 lg:h-48 overflow-hidden flex-shrink-0 transition-all duration-300 rounded-full md:rounded-lg lg:rounded-lg md:mt-0 lg:ml-0">
+        {/* Imagen de perfil */}
+        <section className="w-32 -ml-10 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-48 lg:h-48 overflow-hidden flex-shrink-0 transition-all duration-300 rounded-full md:rounded-lg lg:rounded-lg md:mt-0 lg:mt-10 lg:-ml-16">
           <img
             src="/imagen.jpg"
             alt="Profile"
@@ -11,7 +12,7 @@ export default function Home() {
           />
         </section>
 
-
+        {/* Contenido de texto y botones */}
         <section className="mt-6 md:mt-4 lg:mt-0 space-y-2 text-center md:text-center lg:text-left">
           <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">Jess Wilson</h2>
           <p className="text-sm md:text-base lg:text-lg text-gray-400">UX Engineer</p>
@@ -26,7 +27,7 @@ export default function Home() {
             <button className="bg-white text-black px-4 py-2 rounded-full text-xs md:text-sm lg:text-base hover:bg-gray-200 transition">
               Follow
             </button>
-            </section>
+          </section>
         </section>
       </article>
     </main>
